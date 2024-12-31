94cde2ff572f6f5cceafa20036c8f475ecf1278e
ramkrishna2910 <ramkrishna2910@gmail.com>
2024-12-31 11:13:01 -0800

    uncomment exec call

--- human_eval/execution.py
+++ human_eval/execution.py
@@ -52,7 +52,7 @@ def unsafe_execute(problem: Dict, completion: str, timeout: float, result: List)
                     # information on how OpenAI sandboxes its code, see the accompanying paper.
                     # Once you have read this disclaimer and taken appropriate precautions,
                     # uncomment the following line and proceed at your own risk:
-                    # exec(check_program, exec_globals)
+                    exec(check_program, exec_globals)
             result.append("passed")
         except TimeoutException:
             result.append("timed out")
