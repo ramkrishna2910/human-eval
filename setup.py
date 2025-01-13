29d6b917d151d997cf217f36523211b47e78f084
ramkrishna2910 <ramkrishna2910@gmail.com>
2025-01-13 09:49:37 -0800

    changes for pypi release

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="human-eval",
-    py_modules=["human-eval"],
-    version="1.0.4",
-    description="",
-    author="OpenAI",
+    name="human-eval-windows",
+    py_modules=["human_eval"],
+    version="1.0.0",
+    description="Windows-compatible fork of OpenAI's human-eval",
+    author="ramkrishna2910",
+    author_email="ramkrishna2910@gmail.com",
+    url="https://github.com/ramkrishna2910/human-eval", 
     packages=find_packages(),
     install_requires=[
         str(r)
@@ -21,5 +23,16 @@ setup(
         "console_scripts": [
             "evaluate_functional_correctness = human_eval.evaluate_functional_correctness:main",
         ]
-    }
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.7",
 )
